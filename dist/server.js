3ebd01a16ed704c0c0a933507a4212e2c1292670
Kostya <ckutls228@gmail.com>
2024-08-09 10:10:43 +0300

    test

---
+++ dist/server.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get('/', (req, res) => {
+    return res.send('server work!');
+});
+app.get('/ping', (req, res) => {
+    return res.send('Pong');
+});
+app.listen(3000, () => {
+    console.log('http://localhost:3000');
+});
+//# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,11 @@ import express, { Express, Request, Response } from "express";
 const app: Express = express()
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('server work!')
+    return res.send('server work!')
+})
+
+app.get('/ping', (req: Request, res: Response) => {
+    return res.send('Pong')
 })
 
 app.listen(3000, () => {
